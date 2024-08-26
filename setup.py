35dab18a9a7176e1fd7539c8ca3d01a37b4dacb5
Jun Aishima <jaishima@bnl.gov>
2024-08-26 11:43:45 -0400

    fix package, add package_data for schema files

--- setup.py
+++ setup.py
@@ -54,7 +54,8 @@ setup(
     license="BSD (3-clause)",
     url="https://github.com/NSLS-II/analysisstore.git",
     python_requires=">={}".format(".".join(str(n) for n in min_version)),
-    packages={'analysisstore': ['schemas/*.json']},
+    packages=find_packages(),
+    package_data={'analysisstore': ['schemas/*.json']},
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Development Status :: 3 - Alpha",
